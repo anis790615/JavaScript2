72f9a96513c7deb411014c73cea41fb0e5862472
Anis Alkomem <anis.alkumaim@gmail.com>
2020-04-22 18:18:04 +0200

    Added another solution to ex2

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -13,6 +13,14 @@ does not return anything but removes any duplicate elements from the array.
 // WRITE YOUR FUNCTION HERE
 function removeDuplicates(arr) {
   return arr.filter((value, index) => arr.indexOf(value) === index);
+  // Another solution through modifying the original array
+  // for (let i = arr.length; i > 0; i--) {
+  //   if (arr.indexOf(arr[i]) !== i) {
+  //     // console.log(arr[i]);
+  //     arr.splice(i, 1);
+  //   }
+  // }
+  // return arr;
 }
 
 const letters = ['a', 'b', 'b', 'c', 'd', 'a', 'e', 'f', 'f', 'c', 'b'];
@@ -21,4 +29,4 @@ console.log(removeDuplicates(letters));
 // if (letters === ['a', 'b', 'c', 'd', 'e', 'f']) {
 //   console.log('Hooray!');
 // }
-/* I omitted the above check, first of all, because the modification of the original array is no longer a requirement, and secondly, because, even if I aasigned the output of the function into a new array, the check will always return false, unless they point to the same exact array. */
+// /* I omitted the above check, because the check will always return false, unless they point to the same exact array. */
