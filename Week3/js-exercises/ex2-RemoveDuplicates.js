22a4ec5cad3b82d84e76ae1c1efcaca209e69245
Anis Alkomem <anis.alkumaim@gmail.com>
2020-04-22 15:19:37 +0200

    Finished ex2

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -12,27 +12,13 @@ does not return anything but removes any duplicate elements from the array.
 
 // WRITE YOUR FUNCTION HERE
 function removeDuplicates(arr) {
-  // arr = arr.filter((value, index) => arr.indexOf(value) === index);
-  // return arr;
-  arr.forEach((value, index) => {
-    // console.log(arr[index], value, arr.indexOf(value));
-
-    if (arr.indexOf(value) !== index) {
-      console.log(value);
-      // removeElement(arr, index);
-      arr.splice(index, 1);
-    }
-  });
+  return arr.filter((value, index) => arr.indexOf(value) === index);
 }
 
 const letters = ['a', 'b', 'b', 'c', 'd', 'a', 'e', 'f', 'f', 'c', 'b'];
-// letters = letters.filter((value, index) => letters.indexOf(value) === index);
-
 console.log(removeDuplicates(letters));
-console.log(letters);
-if (letters === ['a', 'b', 'c', 'd', 'e', 'f']) {
-  console.log('Hooray!');
-}
-// function removeElement(arr, index) {
-//   arr.splice(index, 1);
+
+// if (letters === ['a', 'b', 'c', 'd', 'e', 'f']) {
+//   console.log('Hooray!');
 // }
+/* I omitted the above check, first of all, because the modification of the original array is no longer a requirement, and secondly, because, even if I aasigned the output of the function into a new array, the check will always return false, unless they point to the same exact array. */
